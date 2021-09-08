a9a25cba8b1d8db55279f0228fc7149a29d0194b
tristan <tanshilinmail@gmail.com>
2021-09-08 20:45:33 +0800

    1

--- test/test.py
+++ test/test.py
@@ -1,16 +1,52 @@
 import yaml
 
 table_define_fc = None
-application_fc = None
+application = None
+table_define = []  # {"s_d": "","s_t": "","t_d": "","t_t": ""}
+total_table_count = 0
 
 
 def load_config():
     global table_define_fc
-    global application_fc
+    global application
     with open("table_define.txt", "r", encoding="utf-8")as f:
-        table_define_fc = f.read()
+        table_define_fc = f.readlines()
     with open("application.yaml", "r", encoding="utf-8")as f:
-        application_fc = yaml.safe_load(f.read())
+        application = yaml.safe_load(f.read())
+
+
+def gen_table_define():
+    def append_table_define(s_d, s_t, t_d, t_t):
+        global total_table_count
+        table_define.append({"s_d": s_d, "s_t": s_t, "t_d": t_d, "t_t": t_t})
+        total_table_count += 1
+        if t_d in application["db_mapping"]:
+            table_define.append({"s_d": s_d, "s_t": s_t, "t_d": application["db_mapping"][t_d], "t_t": t_t})
+            total_table_count += 1
+
+    revert_table_distribute_merge = {}
+    for key in application["table_distribute_merge"]:
+        for item in application["table_distribute_merge"][key]:
+            revert_table_distribute_merge[item] = key
+    db_name = None
+    table_define_merge_record = {}  # {"m-wms": "wms"}
+    is_skip_db = True
+    for item in table_define_fc:
+        item = item.strip()
+        if "" == item:
+            continue
+        if "(" in item:
+            is_skip_db = False
+            db_name = item[:item.find("(")]
+            if db_name in revert_table_distribute_merge:
+                db_name = revert_table_distribute_merge[db_name]
+                if db_name in table_define_merge_record:
+                    if item == table_define_merge_record[db_name]:
+                        is_skip_db = True
+            continue
+        if is_skip_db:
+            continue
+        append_table_define(db_name, item, db_name, item)
 
 
 def gen_select_sql():
@@ -19,4 +55,7 @@ def gen_select_sql():
 
 if __name__ == '__main__':
     load_config()
+    gen_table_define()
+    print(table_define)
+    print(total_table_count)
     gen_select_sql()
